46595fcda64a889a24121709d2c53389f71b8ccf
jorge <91345902+jreyesgfi@users.noreply.github.com>
2021-11-28 19:42:51 +0100

    Inicio tablero con material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Grid
+} from '@mui/material';
 
 function App() {
   return (
@@ -10,6 +13,22 @@ function App() {
           Sudokus
         </h1>
       </header>
+      <div className="tablero">
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <p>1</p>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <p>2</p>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <p>3</p>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <p>4</p>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
