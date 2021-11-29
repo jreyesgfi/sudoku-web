05aef209e9bf7e903968379572ef5eb1df0f2b66
jorge <91345902+jreyesgfi@users.noreply.github.com>
2021-11-29 07:42:14 +0100

    Parte de la implementación función resaltar

--- src/Elementos/Celda.js
+++ src/Elementos/Celda.js
@@ -1,7 +1,16 @@
+import React from "react";
 export default class Celda{
-    constructor(elementoUi,x,y){
-        this.ui = elementoUi;
+    constructor(x,y,key){
         this.x = x;
         this.y = y;
+        // No podemos crear un hook en una clase
+        this.resaltada = false;
+        this.ui = React.createElement('Box', { className: `celda ${this.resaltada && 'resaltada'}`, key:key});
+    }
+
+
+    setResaltada(resaltada){
+        this.resaltada = resaltada;
+        return this.ui
     }
 }
--- src/Views/Tablero.js
+++ src/Views/Tablero.js
@@ -1,7 +1,8 @@
 import { Box } from '@mui/system';
 import React from 'react';
+import Celda from '../Elementos/Celda';
 export default class Tablero {
-    static cuadradosEnLista = [];
+    static tableroEnLista = [];
 
     static construirTablero() {
         let tablero = null;
@@ -9,35 +10,71 @@ export default class Tablero {
         let celdas = [];
         let key = 0;
 
+        // Creamos el tablero vacío
+        this.tableroEnLista = [];
+        for (let i = 0; i < 9; i++) {
+            this.tableroEnLista.push([]);
+            for (let j = 0; j < 9; j++) {
+                this.tableroEnLista[i].push(j)
+            }
+        }
+
+
         for (let i = 0; i < 9; i++) {
 
-            this.cuadradosEnLista.push([])
+            this.tableroEnLista.push([])
 
             for (let j = 0; j < 9; j++) {
-                // Creamos las celdas
-                let celda = React.createElement('Box', { className: "celda", key:key});
-                console.log(this.cuadradosEnLista[i]);
+
+                // Creamos la celda como información
+                let x = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+                let y = i % 3 * 3 + j % 3;
+                let dataCelda = new Celda(x, y, key)
+
+                // Creamos las celdas gráficas
+                let celda = dataCelda.ui;
                 celdas.push(celda);
-                this.cuadradosEnLista[i].push(celda);
-                // aquí debemos añadir la celda a la lista, debemos tener en cuenta que
-                // la fila no es a priori correcta
+
+                this.tableroEnLista[x][y] = dataCelda;
                 // tambíen podríamos indicar que dicha celda posee un elemento cuadrado asociado
-                console.log(i,j);
+                // console.log(x, y);
                 key++;
             }
 
             // Si estamos en el último hijo creamos el cuadrado
             // Creamos los cuadrados
             let cuadrado = React.createElement('Box',
-                { className: "cuadrado flex-grid ejemplo", key:i},
+                { className: "cuadrado flex-grid ejemplo", key: i },
                 celdas);
             celdas = [];
             cuadrados.push(cuadrado)
         }
+
+        // Prueba de colorear
+        this.colorearCeldas([[0, 3],[1,2]]);
+
+
         return (
             tablero = React.createElement('Box',
                 { className: "tablero flex-grid" },
                 cuadrados)
         );
+    }
+
+    static colorearCeldas(listaDeCoordenadas) {
+        for (let celda of this.tableroEnLista) {
+            celda.resaltada = false;
+        }
+        if (listaDeCoordenadas) {
+            if (listaDeCoordenadas.length > 1) {
+                for (let coord of listaDeCoordenadas) {
+                    console.log(coord[0]);
+                    this.tableroEnLista[coord[0]][coord[1]].resaltada = true;
+                }
+            } else {
+                let coord = listaDeCoordenadas;
+                this.tableroEnLista[coord[0]][coord[1]].resaltada = true;
+            }
+        }
     }
 }
